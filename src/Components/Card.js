9ccc237a5cfd0cb2f388d18d641c55d240329be3
Kartik Mahendra Parab <126079035+kartik9699@users.noreply.github.com>
2024-11-19 16:53:05 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -31,7 +31,7 @@ function Card(props) {
   };
   const handleCheckout = async () => {
     let userEmail = localStorage.getItem('userEmail');
-    let response = await fetch("http://localhost:5000/api/orderData", {
+    let response = await fetch("https://backend-pearl-tau.vercel.app/api/orderData", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
